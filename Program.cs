1c2d82b22ae709fc6dc386e41261e366ac1f2fea
Vahdet Savci <savcivahdet@gmail.com>
2024-01-08 17:52:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace mutlak_kare_alma;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Başlık
+
+## LİSANS
+![MIT](LICENSE)
